a9a7d76d350d69ea72c3961a6e29438826e26b29
mauleyzaola <mauricio.leyzaola@gmail.com>
2023-11-01 07:28:03 -0600

    UX-141 remove useless import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './NavBar'
-import Env from './Env'
 
 const inter = Inter({ subsets: ['latin'] })
 
