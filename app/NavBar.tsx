bc2c8cdde96f1743d6922bf278347b75c4fa29c7
mauleyzaola <mauricio.leyzaola@gmail.com>
2023-11-01 22:26:35 -0600

    UX-141 wip

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,25 @@
 'use client'
 
+import {useSession} from "next-auth/react";
 import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
-  return (
-    <div className="flex p-3 space-x-3">
-        <Link href="/" className="mr-5">Inicio</Link>
-        <Link href="/share">Compartir</Link>
-    </div>
-  )
+    const {status, data: session} = useSession()
+    return (
+        <div className="flex p-3 space-x-3">
+            <Link href="/" className="mr-5">Inicio</Link>
+            <Link href="/share">Compartir</Link>
+            {status === 'loading' && <div>Loading...</div>}
+            {status === 'authenticated' &&
+                <div className="flex space-x-3">
+                    <span>{session.user!.name}</span>
+                    <Link href="/api/auth/signout">Sign Out</Link>
+                </div>
+            }
+            {status === 'unauthenticated' && <Link href="/api/auth/signin">Login</Link>}
+        </div>
+    )
 }
 
 export default NavBar
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-export default function Home() {
-  return (
-    <main className="p-3">
-      <div>
-        Hello Mau!
-      </div>
-    </main>
-  )
+import {getServerSession} from "next-auth";
+import {authOptions} from "./api/auth/[...nextauth]/route";
+
+export default async function Home() {
+    const session = await getServerSession(authOptions)
+    return (
+        <main className="p-3">
+            <div>
+                <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+            </div>
+        </main>
+    )
 }
