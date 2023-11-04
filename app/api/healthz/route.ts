f4f745c35517e1bc0780dd3d2b4f2877487ed9be
mauleyzaola <mauricio.leyzaola@gmail.com>
2023-11-04 16:01:28 -0600

    UX-154 healthz endpoint working

---
+++ app/api/healthz/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    let data;
+    const baseURL = process.env.BASE_API_URL
+    try {
+        await fetch(`${baseURL}/v1/healthz`).
+            then(res => res.json())
+            .then(d => data = d)
+        return NextResponse.json(data)
+    } catch (e) {
+        return NextResponse.json(e)
+    }
+}
