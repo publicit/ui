8f9b6728032783f22d72d020782de183652aa1bb
mauleyzaola <mauricio.leyzaola@gmail.com>
2023-11-05 23:59:38 -0600

    UX-150 wip

--- app/api/auth/AuthOptions.ts
+++ app/api/auth/AuthOptions.ts
@@ -14,14 +14,20 @@ export const authOptions: NextAuthOptions = {
             return baseUrl
         },
         async signIn({ user, account, profile, email, credentials }) {
-            // TODO: POST the user to /v1/users
-            const res = await fetch(`http://localhost:8000/v1/users`, {
-                method: "POST",
-                body: JSON.stringify(user),
-            })
-            const data = await res.json()
-            console.log(`data: ${JSON.stringify(data, null, 2)}`)
-            return true
+            try {
+                console.log(`incoming payload: ${JSON.stringify(user, null, 2)}`)
+                // TODO: POST the user to /v1/users
+                const res = await fetch(`http://localhost:8000/v1/users`, {
+                    method: "POST",
+                    body: JSON.stringify(user),
+                })
+                const data = await res.json()
+                console.log(`data: ${JSON.stringify(data, null, 2)}`)
+                return true
+            } catch (e) {
+                console.log(e)
+                return false
+            }
         },
     },
     session: {
