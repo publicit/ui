e8ae90c70964cf9e82c7f9cb2e4f7be6c80892fb
mauleyzaola <mauricio.leyzaola@gmail.com>
2023-11-06 10:41:13 -0600

    UX-150 remove cache from /api/users endpoint and use env vars for backend host resolution

--- app/api/auth/AuthOptions.ts
+++ app/api/auth/AuthOptions.ts
@@ -1,11 +1,19 @@
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
+const baseApiUrl = process.env.BASE_API_URL
+
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            authorization:{
+                params:{
+                    prompt:"consent",
+                    response_type: "code",
+                }
+            },
         })
     ],
     callbacks: {
@@ -15,8 +23,9 @@ export const authOptions: NextAuthOptions = {
         },
         async signIn({ user, account, profile, email, credentials }) {
             try {
-                // TODO: endpoint host should come from env vars
-                const res = await fetch(`http://localhost:8000/v1/users`, {
+                // we make a request to the backend on each login,
+                // so we have the latest user information in database.
+                const res = await fetch(`${baseApiUrl}/v1/users/login`, {
                     method: "POST",
                     body: JSON.stringify(user),
                 })
