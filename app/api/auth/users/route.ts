4776f38217c3f9b52724381681716da8900b8277
mauleyzaola <mauricio.leyzaola@gmail.com>
2023-11-11 06:57:24 -0600

    UX-150 manually checking for user session

--- app/api/auth/users/route.ts
+++ app/api/auth/users/route.ts
@@ -1,7 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export async function GET(req: NextRequest) {
     const baseURL = process.env.BASE_API_URL
+    const session = await getServerSession()
+    if (!session || !session.user) {
+        return NextResponse.json({
+            error: "unauthorized",
+        })
+    }
     try {
         const res = await fetch(`${baseURL}/v1/users-list`, {
             method: "POST",
