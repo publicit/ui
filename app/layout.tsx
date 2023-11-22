8bc2059c730e8885c0e7710bb41fcf3bcacff053
mauleyzaola <mauricio.leyzaola@gmail.com>
2023-11-22 09:56:17 -0600

    UX-91 simplified layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,22 @@
 import type {Metadata} from "next";
-import {Inter} from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
 import AuthProvider from "./api/auth/Provider";
-
-const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
     title: "Publicit UX",
     description: "",
 };
-const env = process.env;
-const versionInfo = {
-    version: env["NEXT_PUBLIC_TAG_NAME"],
-    hash: env["NEXT_PUBLIC_GIT_COMMIT"],
-};
-if (versionInfo.version) {
-    // TODO: make this work with nextjs deployments
-    console.table(versionInfo);
-}
 
-export default function RootLayout({
-                                       children,
-                                   }: {
+export default function RootLayout({children}: {
     children: React.ReactNode;
 }) {
     return (
         <html lang="en" data-theme="dark">
         <AuthProvider>
-            <body className={inter.className}>
-                <NavBar/>
-                <div className="content-baseline">{children}</div>
+            <body>
+            <NavBar/>
+            <div className="content-baseline">{children}</div>
             </body>
         </AuthProvider>
         </html>
