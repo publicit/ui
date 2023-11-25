5bcb7633a1db157014a9d7a35917658c8a233f07
mauleyzaola <mauricio.leyzaola@gmail.com>
2023-11-25 12:44:50 -0600

    UX-159 mantine 7 with collapsible navbar using burger component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react'
 import {TokenResponse, useGoogleLogin} from '@react-oauth/google';
 import {logout, parseToken, saveUserProfile} from "./helpers/sso_service"
-import {Text} from "@mantine/core";
+import "@mantine/core/styles.css"
+import {AppShell, Burger, Button} from "@mantine/core";
+import {useDisclosure} from "@mantine/hooks";
 
 type UserProfile = {
     picture: string
@@ -38,22 +40,59 @@ function App() {
         logout()
         setProfile(undefined);
     };
+
+    function CollapseDesktop() {
+        const [mobileOpened, {toggle: toggleMobile}] = useDisclosure();
+        const [desktopOpened, {toggle: toggleDesktop}] = useDisclosure(true);
+        const [opened, {toggle}] = useDisclosure()
+
+
+        return (
+            <AppShell
+                padding="md"
+                header={{height: 60}}
+                navbar={{
+                    width: 200,
+                    breakpoint: 'sm',
+                    collapsed: {mobile: !mobileOpened, desktop: !desktopOpened},
+                }}
+            >
+                <AppShell.Header>
+                    <Burger opened={opened} onClick={toggleDesktop} aria-label="Toggle navigation"/>
+                    <div>Logo</div>
+                </AppShell.Header>
+                <AppShell.Navbar>Navbar</AppShell.Navbar>
+                <AppShell.Main>
+                    <GoogleSection/>
+                </AppShell.Main>
+            </AppShell>
+        );
+    }
+
+    function GoogleSection() {
+        return (
+            <div>
+                <br/>
+                {profile ? (
+                    <div>
+                        <img src={profile.picture} alt={profile.name}/>
+                        <h3>User Logged in</h3>
+                        <p>Name: {profile.name}</p>
+                        <p>Email Address: {profile.email}</p>
+                        <Button variant="outline" onClick={logOut}>Log out</Button>
+                    </div>
+                ) : (
+                    <Button variant="outline" onClick={() => login()}>Sign in with Google</Button>
+                )}
+                <p>Just a dummy text to make sure nothing overlaps the footer</p>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            <h2>App</h2>
-            <Text>Just some text</Text>
-            {profile ? (
-                <div>
-                    <img src={profile.picture} alt={profile.name}/>
-                    <h3>User Logged in</h3>
-                    <p>Name: {profile.name}</p>
-                    <p>Email Address: {profile.email}</p>
-                    <button onClick={logOut}>Log out</button>
-                </div>
-            ) : (
-                <button onClick={() => login()}>Sign in with Google</button>
-            )}
-        </div>
+        <>
+            <CollapseDesktop/>
+        </>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,28 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {GoogleOAuthProvider} from "@react-oauth/google";
-import {MantineProvider} from "@mantine/core";
-import {MantineThemeOverride} from "@mantine/styles/lib/theme/types";
+import {createTheme, MantineProvider} from "@mantine/core";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
-
-const theme: MantineThemeOverride = {
-    colorScheme: "dark",
-    fontFamily: "Tahoma,Ubuntu,Georgia",
-    fontSizes: {},
-    globalStyles: (theme) => ({
-        body: {
-            ...theme.fn.fontStyles(),
-            backgroundColor:
-                theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.white,
-            color:
-                theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
-            lineHeight: theme.lineHeight,
-        },
-    }),
-}
 
 
 // read google credentials from environment
@@ -33,9 +16,14 @@ if (!clientId) {
     throw new Error("missing clientId")
 }
 
+
+const theme = createTheme({
+    //  override as needed
+})
+
 root.render(
     <GoogleOAuthProvider clientId={clientId}>
-        <MantineProvider theme={theme}>
+        <MantineProvider theme={theme} defaultColorScheme="dark">
             <React.StrictMode>
                 <App/>
             </React.StrictMode>
