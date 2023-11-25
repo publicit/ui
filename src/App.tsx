cc4489a15bf20b23cd0f274b90cade8d714342a7
mauleyzaola <mauricio.leyzaola@gmail.com>
2023-11-25 09:07:32 -0600

    UX-159 sso service helper

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useEffect, useState} from 'react'
 import './App.css';
+import {TokenResponse, useGoogleLogin} from '@react-oauth/google';
+import {logout, parseToken, saveUserProfile} from "./helpers/sso_service"
+
+type UserProfile = {
+    picture: string
+    name: string
+    email: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // user contains the access token information, and expiration
+    const [user, setUser] = useState<TokenResponse>();
+    // profile contains the parsed information after we verify the access token with Google endpoint
+    const [profile, setProfile] = useState<UserProfile>();
+
+    const login = useGoogleLogin({
+        onSuccess: (codeResponse) => {
+            setUser(codeResponse)
+            saveUserProfile(codeResponse)
+        },
+        onError: (error) => console.log('Login Failed:', error)
+    });
+
+    useEffect(
+        () => {
+            parseToken()
+                .then(p => setProfile(p))
+                .catch(() => {
+                });
+        },
+        [user]
+    );
+
+    const logOut = () => {
+        logout()
+        setProfile(undefined);
+    };
+    return (
+        <div>
+            <h2>React Google Login</h2>
+            {profile ? (
+                <div>
+                    <img src={profile.picture} alt={profile.name}/>
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <button onClick={logOut}>Log out</button>
+                </div>
+            ) : (
+                <button onClick={() => login()}>Sign in with Google</button>
+            )}
+        </div>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {GoogleOAuthProvider} from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+// read google credentials from environment
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || ""
+if (!clientId) {
+    throw new Error("missing clientId")
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <GoogleOAuthProvider clientId={clientId}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
