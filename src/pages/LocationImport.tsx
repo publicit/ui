f96da7c44ca784d0c6757fcc96ca250fbd481aca
mauleyzaola <mauricio.leyzaola@gmail.com>
2024-01-14 11:25:12 -0600

    UX-224 added input file for csv file upload

--- src/pages/LocationImport.tsx
+++ src/pages/LocationImport.tsx
@@ -1,7 +1,20 @@
+import {FileInput} from "@mantine/core";
+import {IconUpload} from "@tabler/icons-react";
+
 export default function LocationImport() {
     return (
         <div>
             Haz click <a href="/assets/location-import-template.csv" target="_blank">aqui</a> para descargar el CSV de muestra.
+            <br/>
+            <FileInput
+                placeholder="Archivo CSV"
+                label="Subir datos de localizaciones"
+                multiple={false}
+                onChange={file => console.log(file)}
+                leftSection={<IconUpload/>}
+                clearable={true}
+                accept=".csv"
+            />
         </div>
     )
 }
