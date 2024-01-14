a5f843b1a205c7360658d548428a8806d8b016bf
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2024-01-13 23:54:33 -0600

    UX-223 added link to csv template file

---
+++ src/pages/LocationImport.tsx
@@ -0,0 +1,7 @@
+export default function LocationImport() {
+    return (
+        <div>
+            Haz click <a href="/assets/location-import-template.csv" target="_blank">aqui</a> para descargar el CSV de muestra.
+        </div>
+    )
+}
