cd4c31f7bf2e99bc2f4336d5759aeb3804f29b57
Osama Aslam <osamaaslam029@gmail.com>
2024-02-02 22:31:07 +0500

    Implement a 404 page for missing routes #59 is fixed (#60)

--- src/RouteSwitcher.tsx
+++ src/RouteSwitcher.tsx
@@ -1,8 +1,8 @@
 import Root from "./pages/Root";
-import {Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import ErrorPage from "./pages/ErrorPage";
 import CampaignList from "./pages/CampaignList";
-import {UserProfile} from "./models/user";
+import { UserProfile } from "./models/user";
 import CampaignEdit from "./pages/CampaignEdit";
 import CampaignNew from "./pages/CampaignNew";
 import QuizNew from "./pages/QuizNew";
@@ -16,39 +16,39 @@ import UserQuizList from "./pages/UserQuizList"
 import UserQuizFillForm from "./pages/UserQuizFillForm";
 import UserQuizSummaryView from "./pages/UserQuizSummaryView";
 import ShareStart from "./pages/UserQuizSharedStart";
-import {RolesList} from "./pages/RolesList";
-import {RoleEdit} from "./pages/RoleEdit";
-import {UsersList} from "./pages/UserList";
-import {UserEdit} from "./pages/UserEdit";
+import { RolesList } from "./pages/RolesList";
+import { RoleEdit } from "./pages/RoleEdit";
+import { UsersList } from "./pages/UserList";
+import { UserEdit } from "./pages/UserEdit";
 
 type params = {
     profile: UserProfile | undefined
 }
 
-export default function RouteSwitcher({profile}: params) {
+export default function RouteSwitcher({ profile }: params) {
     return (
         <Routes>
-            <Route path="/" element={<Root/>}/>
-            <Route path="/answers/new/:question_id" element={<AnswerNew/>}/>
-            <Route path="/answers/:id" element={<AnswerEdit/>}/>
-            <Route path="/campaigns" element={<CampaignList/>}/>
-            <Route path="/campaigns/new" element={<CampaignNew/>}/>
-            <Route path="/campaigns/:id" element={<CampaignEdit/>}/>
-            <Route path="/questions/new/:quiz_id" element={<QuestionNew/>}/>
-            <Route path="/questions/:id" element={<QuestionEdit/>}/>
-            <Route path="/quizs/new/:campaign_id" element={<QuizNew/>}/>
-            <Route path="/quizs/:id" element={<QuizEdit/>}/>
-            <Route path="/roles" element={<RolesList/>}/>
-            <Route path="/roles/:id" element={<RoleEdit/>}/>
-            <Route path="/user/profile" element={<ProfileEdit/>}/>
-            <Route path="/user/quizs" element={<UserQuizList/>}/>
-            <Route path="/user/quizs/:id" element={<UserQuizFillForm/>}/>
-            <Route path="/user/quizs/:user_quiz_id/summary" element={<UserQuizSummaryView/>}/>
-            <Route path="/users" element={<UsersList/>}/>
-            <Route path="/users/:id" element={<UserEdit/>}/>
-            <Route path="/invitation/:token" element={<ShareStart/>}/>
+            <Route path="/" element={<Root />} />
+            <Route path="/answers/new/:question_id" element={<AnswerNew />} />
+            <Route path="/answers/:id" element={<AnswerEdit />} />
+            <Route path="/campaigns" element={<CampaignList />} />
+            <Route path="/campaigns/new" element={<CampaignNew />} />
+            <Route path="/campaigns/:id" element={<CampaignEdit />} />
+            <Route path="/questions/new/:quiz_id" element={<QuestionNew />} />
+            <Route path="/questions/:id" element={<QuestionEdit />} />
+            <Route path="/quizs/new/:campaign_id" element={<QuizNew />} />
+            <Route path="/quizs/:id" element={<QuizEdit />} />
+            <Route path="/roles" element={<RolesList />} />
+            <Route path="/roles/:id" element={<RoleEdit />} />
+            <Route path="/user/profile" element={<ProfileEdit />} />
+            <Route path="/user/quizs" element={<UserQuizList />} />
+            <Route path="/user/quizs/:id" element={<UserQuizFillForm />} />
+            <Route path="/user/quizs/:user_quiz_id/summary" element={<UserQuizSummaryView />} />
+            <Route path="/users" element={<UsersList />} />
+            <Route path="/users/:id" element={<UserEdit />} />
+            <Route path="/invitation/:token" element={<ShareStart />} />
 
-            <Route path="*" element={<ErrorPage/>}/>
+            <Route path="*" element={<ErrorPage />} />
         </Routes>
     )
 }
