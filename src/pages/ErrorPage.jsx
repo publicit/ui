1826bd5facbef0b99d39365768350ec812a1cfd1
osama <osamaaslam029@gmail.com>
2024-02-02 21:36:52 +0500

    Implement a 404 page for missing routes #59 is fixed

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,15 +1,9 @@
-import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
-    const error = useRouteError();
-
     return (
         <div id="error-page">
             <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
+            <p>Esta pagina no existe</p>
         </div>
     );
 }
