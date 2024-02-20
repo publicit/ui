eba555793794edf5135afbd062eb2f2ad99dff0a
Osama Aslam <osamaaslam029@gmail.com>
2024-02-21 00:49:31 +0500

    UX-287-sidebar-menu-doesnot-scroll-with-mouse-wheel fixed + designed nav and sidebar (#78)
    * UX-287-sidebar-menu-doesnot-scroll-with-mouse-wheel fixed + designed nav and sidebar
    
    * add scroll bar + character limit on user email account
    
    ---------
    
    Co-authored-by: Mauricio Leyzaola <mauleyzaola@users.noreply.github.com>

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,13 +1,26 @@
-import {Avatar, Group, rem, Text, UnstyledButton} from '@mantine/core';
-import {IconChevronRight} from '@tabler/icons-react';
+import { IconChevronRight } from '@tabler/icons-react';
+
+// Mantine :
+import { Avatar, Group, rem, Text, UnstyledButton } from '@mantine/core';
+
+// Models :
+import { UserProfileResponse } from "../models/user"
+
+// CSS :
 import classes from './UserButton.module.css';
-import {UserProfileResponse} from "../models/user"
+
 
 type params = {
     user: UserProfileResponse
 }
 
-export function UserButton({user}:params) {
+export function UserButton({ user }: params) {
+    const userEmail = user.email.length > 24;
+
+    const truncatedName = userEmail
+        ? `${user.email.slice(0, 12)}...${user.email.slice(-10)}`
+        : user.email;
+
     return (
         <UnstyledButton className={classes.user}>
             <Group>
@@ -15,17 +28,10 @@ export function UserButton({user}:params) {
                     src={user.picture}
                     radius="xl"
                 />
-
                 <div style={{ flex: 1 }}>
-                    <Text size="sm" fw={500}>
-                        {user.name}
-                    </Text>
-
-                    <Text c="dimmed" size="xs">
-                        {user.email}
-                    </Text>
+                    <Text size="sm" fw={500}>{user.name}</Text>
+                    <Text c="dimmed" size="xs">{truncatedName}</Text>
                 </div>
-
                 <IconChevronRight style={{ width: rem(14), height: rem(14) }} stroke={1.5} />
             </Group>
         </UnstyledButton>
