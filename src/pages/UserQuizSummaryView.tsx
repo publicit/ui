f8dc5a316396ec22cc2e2d4817543686812aa6b1
Mauricio Leyzaola <mauleyzaola@users.noreply.github.com>
2024-02-27 12:50:05 -0600

    UX-302 style a bit the shares table and include share removal (#85)

--- src/pages/UserQuizSummaryView.tsx
+++ src/pages/UserQuizSummaryView.tsx
@@ -2,7 +2,13 @@ import {useNavigate, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {notifyErrResponse} from "../components/Errors";
 import {UserQuiz, UserQuizStatus} from "../models/user_quiz";
-import {GetUserQuizSummary, PostUserQuizRetry, UserQuizShareLink, UserQuizShareList} from "../helpers/api";
+import {
+    GetUserQuizSummary,
+    PostUserQuizRetry,
+    UserQuizShareDelete,
+    UserQuizShareLink,
+    UserQuizShareList
+} from "../helpers/api";
 import {UserQuestion} from "../models/user_question";
 import QuizSummary from "../components/QuizSummary";
 import {ShowDialog} from "../components/UserQuizShareDialog"
@@ -17,18 +23,22 @@ export default function UserQuizSummaryView() {
     const [userQuiz, setUserQuiz] = useState<UserQuiz>(new UserQuiz())
     const [userQuestions, setUserQuestions] = useState<UserQuestion[]>([])
     const [sharedUrl, setSharedUrl] = useState("")
-    const [rows,setRows]=useState<UserQuizShare[]>([])
+    const [rows, setRows] = useState<UserQuizShare[]>([])
 
     async function loadData(id: string) {
         try {
             const res = await GetUserQuizSummary(userQuizId)
             setUserQuiz(res.user_quiz)
             setUserQuestions(res.user_questions)
-            const sharedData = await UserQuizShareList(res.user_quiz.quiz.id)
-            setRows(sharedData)
+            await loadShares(res.user_quiz?.quiz?.id)
         } catch (err) {
             await notifyErrResponse(err)
         }
+    }
+
+    async function loadShares(quizId: string) {
+        const sharedData = await UserQuizShareList(quizId)
+        setRows(sharedData)
     }
 
     useEffect(() => {
@@ -49,6 +59,17 @@ export default function UserQuizSummaryView() {
             const res = await UserQuizShareLink(userQuiz.quiz.id)
             const tokenUrl = quizTokenShareUrl(res)
             setSharedUrl(tokenUrl)
+            await loadShares(userQuiz.quiz.id)
+        } catch (error) {
+            await notifyErrResponse(error)
+        }
+    }
+
+    async function onDelete(item: UserQuizShare) {
+        if (!window.confirm(`Eliminar esta invitacion?`)) return
+        try {
+            await UserQuizShareDelete(item.id, item.quiz.id)
+            await loadShares(userQuiz.quiz.id)
         } catch (error) {
             await notifyErrResponse(error)
         }
@@ -59,14 +80,15 @@ export default function UserQuizSummaryView() {
         <>
             <QuizSummary userQuiz={userQuiz} userQuestions={userQuestions} onRetry={retryQuiz}/>
             <br/>
-            <h2>Con quien has compartido esta encuesta</h2>
-            <UserQuizShareTable rows={rows} />
+            <h2>Con quien compartes esta encuesta</h2>
+            <UserQuizShareTable rows={rows} onDelete={onDelete}/>
             <br/>
             {userQuiz.status === UserQuizStatus[UserQuizStatus.success] &&
                 <ShowDialog
                     children={ShareDialogBody({
                         sharedUrl,
-                        onClick:() => {},
+                        onClick: () => {
+                        },
                         text: "Se ha copiado la direccion de la invitacion",
                     })}
                     onClose={() => setSharedUrl("")} onOpen={shareQuiz}/>
