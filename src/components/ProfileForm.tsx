8536d09998a9e0ca076f14e6b393976658b2bba2
mauleyzaola <mauricio.leyzaola@gmail.com>
2024-03-07 04:34:37 -0600

    UX-334 let ine data determine profile

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -1,16 +1,16 @@
-import { IconCheck, IconUpload, IconX } from "@tabler/icons-react";
+import {IconUpload} from "@tabler/icons-react";
 
 // Mantine :
-import { UseFormReturnType } from "@mantine/form";
-import { DatePickerInput } from "@mantine/dates";
-import { Button, FileInput, Grid, Select, Text, TextInput, Title } from "@mantine/core";
+import {UseFormReturnType} from "@mantine/form";
+import {DatePickerInput} from "@mantine/dates";
+import {Button, FileInput, Grid, Select, Text, TextInput} from "@mantine/core";
 
 // Models :
-import { FileType } from "../models/file_item";
-import { UserGender, UserProfile, UserProfileFile } from "../models/user_profile";
+import {FileType} from "../models/file_item";
+import {UserGender, UserProfile, UserProfileFile} from "../models/user_profile";
 
 // Helpers :
-import { capitalizeAllWords } from "../helpers/text_utils";
+import {capitalizeAllWords} from "../helpers/text_utils";
 
 
 type params = {
@@ -31,15 +31,15 @@ const userGender: string[] = [
 ]
 
 export default function ProfileForm({
-    onSubmit,
-    form,
-    email,
-    isCompleted,
-    onFileSelected,
-    saveEnabled,
-    fileTypes,
-    files,
-}: params) {
+                                        onSubmit,
+                                        form,
+                                        email,
+                                        isCompleted,
+                                        onFileSelected,
+                                        saveEnabled,
+                                        fileTypes,
+                                        files,
+                                    }: params) {
     return (
         <form
             onSubmit={form.onSubmit(async (data: any) => {
@@ -51,48 +51,48 @@ export default function ProfileForm({
             <Grid gutter={10}>
                 <Grid.Col span={6}>
                     <TextInput label="Nombre(s)" size="md"
-                        placeholder="Nombre(s)"
-                        disabled={isCompleted}
-                        {...form.getInputProps("first_name")}
+                               placeholder="Nombre(s)"
+                               disabled
+                               {...form.getInputProps("first_name")}
                     />
                 </Grid.Col>
                 <Grid.Col span={6}>
                     <TextInput label="Apellidos" size="md"
-                        placeholder="Apellidos"
-                        disabled={isCompleted}
-                        {...form.getInputProps("last_name")}
+                               placeholder="Apellidos"
+                               disabled
+                               {...form.getInputProps("last_name")}
                     />
                 </Grid.Col>
 
                 <Grid.Col span={6}>
                     <TextInput label="Email" size="md"
-                        value={email}
-                        disabled
+                               value={email}
+                               disabled
                     />
                 </Grid.Col>
 
                 <Grid.Col span={6}>
                     <TextInput label="Telefono" size="md"
-                        placeholder="Telefono"
-                        disabled={isCompleted}
-                        {...form.getInputProps("phone_number")}
+                               placeholder="Telefono"
+                               disabled={isCompleted}
+                               {...form.getInputProps("phone_number")}
                     />
                 </Grid.Col>
 
                 <Grid.Col span={6}>
                     <Text>Fecha de Nacimiento</Text>
                     <DatePickerInput size="md"
-                        disabled={isCompleted}
-                        {...form.getInputProps(`dob`)}
+                                     disabled
+                                     {...form.getInputProps(`dob`)}
                     />
                 </Grid.Col>
                 <Grid.Col span={6}>
                     <Select
-                        label="Sexo" size="md"
+                        label="Genero" size="md"
                         data={userGender}
-                        comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
+                        comboboxProps={{transitionProps: {transition: 'pop', duration: 200}}}
                         {...form.getInputProps("gender")}
-                        disabled={isCompleted}
+                        disabled
                     />
                 </Grid.Col>
                 <Grid.Col span={12}>
@@ -101,20 +101,17 @@ export default function ProfileForm({
                         if (!file) {
                             return null
                         }
-                        const iconRight = !!file.is_valid ? <IconCheck style={{ color: "green" }} /> :
-                            <IconX style={{ color: "red" }} />
                         return (
                             <div key={file.id}>
                                 <FileInput size="md"
-                                    accept="image/*"
-                                    multiple={false}
-                                    clearable={true}
-                                    rightSection={iconRight}
-                                    disabled={!!file.is_valid}
-                                    leftSection={<IconUpload />}
-                                    placeholder={file.file?.name}
-                                    label={capitalizeAllWords(fileType.description)}
-                                    onChange={file => onFileSelected(file, fileType.name)}
+                                           accept="image/*"
+                                           multiple={false}
+                                           clearable={true}
+                                           disabled={!!file.is_valid}
+                                           leftSection={<IconUpload/>}
+                                           placeholder={file.file?.name}
+                                           label={capitalizeAllWords(fileType.description)}
+                                           onChange={file => onFileSelected(file, fileType.name)}
                                 />
                             </div>
                         )
