6514adf2f847fa081cdce5c11037a464d2c4379d
Mauricio Leyzaola <mauricio.leyzaola@gmail.com>
2024-07-26 16:59:46 -0600

    UX-471 implemented registration form

--- src/components/UserQuizRegisterForm.tsx
+++ src/components/UserQuizRegisterForm.tsx
@@ -1,7 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 // Mantine :
-import { Button } from '@mantine/core';
+import { Button, Loader } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import { QuizRegisterInvitation } from '../helpers/api';
+import { notifyErrResponse } from './Errors';
 
 type params = {
   token: string;
@@ -13,11 +16,11 @@ export function QuizRegisterForm({ token }: params) {
     <div className="quiz-unregister-form">
       <div className="form-description">
         <h1 className="heading">
-          Antes de responder la encuesta, es necesario que te registres!
+          Antes de responder la encuesta, es necesario que te registres.
         </h1>
         <div>
-          Haz click en CONTINUAR para acceder a tu registro. Te tomara 5 minutos
-          el proceso.
+          Haz click en INICIAR REGISTRO para completar este paso. Te tomara 5
+          minutos el proceso.
         </div>
         <div></div>
         <Button
@@ -45,6 +48,38 @@ export function QuizUnregisteredForm({ token }: params) {
             Ya casi estás ahí. Haz clic en el botón de abajo para INICIAR SESIÓN
           </div>
         </div>
+      </div>
+    </div>
+  );
+}
+
+export function QuizNoProfileNeededForm({ token }: params) {
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    async function loadData(id: string) {
+      try {
+        if (!token) throw Error('missing token');
+        //  call the server api to validate the token
+        await QuizRegisterInvitation(token || '');
+        // if all goes well, redirect to the user's quiz page
+        navigate(`/user/quizs`);
+      } catch (err) {
+        await notifyErrResponse(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    loadData(token);
+  }, []);
+
+  return (
+    <div className="quiz-unregister-form">
+      <div className="form-description">
+        <h1 className="heading">Estamos procesando la invitacion.</h1>
+        <div>Por favor espera un momento.</div>
+        {isLoading && <Loader color="blue" size="md" />}
       </div>
     </div>
   );
